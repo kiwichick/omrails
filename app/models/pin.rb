c8b45a59bab17c40bcf18d8db2b6a9441896d885
Kylie Baker <kyliebakeacake@gmail.com>
2013-04-01 19:48:58 -0700

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true, length: { less_than: 50 }
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
